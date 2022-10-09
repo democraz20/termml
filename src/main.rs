f4dd05f435b38186d79b8542aaae235878672d47
democraz20 <77766968+democraz20@users.noreply.github.com>
2022-10-09 22:46:16 +0700

    trying out crates

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use colored::*;
-use scraper::{Html, Selector};
+use easy_scraper::Pattern;
 
 fn main() {
-    let html_text = r#"<h1>Hello, World!<div>waow</div></h1> <h1>waow</h1>"#;
-    let fragment = Html::parse_fragment(html_text);
-    let h1_selector = Selector::parse("h1").unwrap();
-    let h1 = fragment.select(&h1_selector).next().unwrap();
-    let h1_text = h1.text().collect::<Vec<_>>();
-
-    println!("{:?}", h1_text);
-
-    // let element = fragment.select(&h1Selector);
-    // let text: String = r#"<a color.red title.true>hello world<\a>"#.to_string();
-    // println!("{}", text);
-
+    //reserve tag : "body" to wrap the whole thing and maybe "head"
+    let html_text = r#"
+    <body>
+        Hello, World!
+        <div>Some div</div>
+        Goodbye
+    </body>
+    "#;
+    // let pat = Pattern::new(r#"<div class="{{class_name}}">{{mes}}</div>"#).unwrap();
+    let pat1 = Pattern::new(r#"<body>{{body_text:*}}</body>"#).unwrap();
+    let first_match = pat1.matches(html_text);
+    dbg!("{}", first_match);
+    // println!("{}", ms[0]["mes"]);
+    // println!("class=\"{}\"", ms[0]["class_name"]);
     println!("{}, {}!", "Hello".red(), "World".green().bold());
 }
