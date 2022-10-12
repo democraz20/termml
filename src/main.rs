aebf8170bbb81c97e481cf2db7626d5a54f69b70
democraz20 <77766968+democraz20@users.noreply.github.com>
2022-10-12 23:40:33 +0700

    some structs for attributes html

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use colored::*;
-use easy_scraper::Pattern;
+
+
+#[derive(Debug)]
+struct HtmlStruct {
+    tag: String,
+    //attributes
+    class: String,
+    link: String,
+    text: String,
+}
+impl HtmlStruct {
+    fn new(tag: String, class: String, link: String, text: String) -> HtmlStruct{
+        HtmlStruct {
+            tag, class, link, text
+        }
+    }
+}
 
 fn main() {
     //reserve tag : "body" to wrap the whole thing and maybe "head"
@@ -10,11 +26,25 @@ fn main() {
         Goodbye
     </body>
     "#;
+    let data = HtmlStruct::new(
+        "div".to_string(),
+        "".to_string(),
+        "".to_string(),
+        "Hello, World!".to_string()
+    );
+    //which is better?
+    // let data = HtmlStruct::new {
+    //     tag: "div".to_string(), 
+    //     class: "".to_string(),
+    //     link: "".to_string(), 
+    //     text: "Hello, World!".to_string()
+    // };
+
+    println!("{}, {}, {}, {}", data.tag, data.class, data.link, data.text);
+
+    dbg!("{}",data);
+
     // let pat = Pattern::new(r#"<div class="{{class_name}}">{{mes}}</div>"#).unwrap();
-    let pat1 = Pattern::new(r#"<body>{{body_text:*}}</body>"#).unwrap();
-    let first_match = pat1.matches(html_text);
-    dbg!("{}", first_match);
-    // println!("{}", ms[0]["mes"]);
     // println!("class=\"{}\"", ms[0]["class_name"]);
     println!("{}, {}!", "Hello".red(), "World".green().bold());
 }
