aa87966325493dc9a9fdef8248081c61fe699461
democraz20 <mozartrasig@gmail.com>
2022-11-10 17:59:43 +0700

    RGB from string (a tuple of 3 u8's)
    with error propagation and still matching in a matching arm to do

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,7 @@ mod static_data;
 use cap::Cap;
 use std::alloc;
 
-use ansi_term::Colour::{
-    Red,
-    Green,
-};
+use ansi_term::Colour::*;
 
 #[global_allocator]
 static ALLOCATOR: Cap<alloc::System> = Cap::new(alloc::System, usize::max_value());
@@ -17,35 +14,12 @@ static ALLOCATOR: Cap<alloc::System> = Cap::new(alloc::System, usize::max_value(
 const INDEX_FILENAME: &str = "index.termml";
 
 fn main() {
-    //enable ansi mode
-    // let enabled = ansi_term::enable_ansi_support();
-    // drop(enabled); //returns a result, no longer used, no error propagation, can be dropped
-    //might not even need to enable ansi at all?
     println!("This is in Red and Green: {}, {}", Red.paint("Hello"), Green.paint("World!"));
-    //reserve tag : "body" to wrap the whole thing and maybe "head"
     // ALLOCATOR.set_limit(30 * 1024 * 1024).unwrap();
-    // let html_text = r#"
-    // <body>
-    //     Hello, World!
-    //     <div>Some div</div>
-    //     Goodbye
-    // </body>
-    // "#;
     let parsed = process_string::serialize::get_index_mark_up(INDEX_FILENAME);
 
-    // let styles = match process_string::serialize::get_styles_mark_up(&parsed.stylesheet.as_ref().unwrap()) {
-    //     Ok(styles) => styles,
-    //     Err(e) => {
-    //         panic!("failed to serialize styles : {}", e) //return an empty stylesheet here ad log for error
-    //     }
-    // };
-    println!("{}", parsed.doctype);
     dbg!(parsed);
-    // dbg!(styles);
-    // dbg!(styles);
-    // process_text(r#"<div class="test" link="github.com">text<waow></div>"#.to_string());
 }
-//input will be ex: <div>text</div>
 
 fn _printallocd(header: &str) -> () {
     //only for development : not suppossed to be in actual build AT ALL
--- src/static_data/term_style.rs
+++ src/static_data/term_style.rs
@@ -3,6 +3,7 @@ use ansi_term::Colour;
 
 fn get_color_from_string(text: &str) -> Colour {
     let text: &str = &text.to_lowercase();
+    let text: &str = &text.replace(" ", "");
     match text {
         "black" => Colour::Black,
         "red" => Colour::Red,
@@ -15,3 +16,28 @@ fn get_color_from_string(text: &str) -> Colour {
         _ => Colour::White
     }
 }
+
+fn get_rgb_from_string(text: &str) -> 
+    Result<(u8, u8, u8), Box<dyn std::error::Error>> {
+    let split = text.split(",");
+    let mut vec = split.map(String::from).collect::<Vec<_>>();
+    if !vec.len() == 3 {
+        return Err(Box::from("RGB values count is not 3"))
+    };
+    for i in 0..3{
+        vec[i] = vec[i].replace(" ", "");
+    }
+    let n1 = vec[0].parse::<u8>()?;
+    let n2 = vec[0].parse::<u8>()?;
+    let n3 = vec[0].parse::<u8>()?;
+    Ok((n1, n2, n3))
+}
+
+//checking windows cmd.exe style support
+//aka Windows Command Prompt
+//[x] colors
+//[x] underline
+//[ ] bold
+//[x] background color
+//[x] RGB values (not tested on all terminals)
+//[x] Fixed xterm-256 values (https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg)
