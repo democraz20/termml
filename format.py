f232cc4886082a34f80285d7a18adf823c9987fe
democraz20 <mozartrasig@gmail.com>
2023-01-14 15:31:18 +0700

    neater formatter code

--- format.py
+++ format.py
@@ -4,10 +4,12 @@ path = '.\\'
 extension = '.rs'
 ignore_dir = ['target']
 for dirpath, dirs, files in os.walk(path):
+    trimmed_path = dirpath.replace(path, '')
     for i in ignore_dir:
-        trimmed_path = dirpath.replace(path, '')
-        if trimmed_path.startswith('\\'+i) == False:
+        if trimmed_path.startswith('\\'+i):
+            continue
+        else:
             for file in files:
                 if file.endswith(extension):
                     os.system(f'rustfmt {dirpath}\{file}')
-                    print(f'formatted : {dirpath}\{file}')
+                    print(f'formatted : {dirpath}\{file}') 
