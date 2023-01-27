1df2ae475bc646f682e01141ab12025999c2e3bd
democraz20 <mozartrasig@gmail.com>
2023-01-27 21:32:08 +0700

    finished logger

--- debug/src/logger.rs
+++ debug/src/logger.rs
@@ -9,13 +9,13 @@ pub struct Logger {
 }
 
 impl Logger {
-    pub fn new(&self, title: &str, path: &str, resave: bool) -> Logger {
+    pub fn new(title: &str, path: &str, resave: bool) -> Logger {
         //clean text input
         let mut title = title.replace("\n", "");
         title = title.replace("\r", "");
 
         Logger {
-            contents: vec![self.log_title.to_string()],
+            contents: vec![format!("[[{}]]",title.to_string())],
             log_title: title.to_string(),
             save_path: path.to_string(),
             re_save: resave
@@ -26,12 +26,15 @@ impl Logger {
         match self.re_save {
             true => {
                 if Path::new(&self.save_path).exists() {
-                    fs::remove_file(self.save_path.to_string())?;
-                    fs::File::create(self.save_path.to_string())?;
                     fs::write(self.save_path.to_string(), content)?;
                     //fs::write writes the entire content of the file
                     //clear the log buffer
-                    self.contents = vec![self.log_title.to_string()]
+                    self.contents = vec![format!("[[{}]]",self.log_title)];
+                }
+                else {
+                    fs::File::create(&self.save_path)?;
+                    fs::write(self.save_path.to_string(), content)?;
+                    self.contents = vec![format!("[[{}]]",self.log_title)];
                 }
             }
             false => {
@@ -39,6 +42,10 @@ impl Logger {
                 if Path::new(&self.save_path).exists() {
                     fs::write(self.save_path.to_string(), content)?;
                     //fs::write writes the entire content of the file
+                }
+                else {
+                    fs::File::create(&self.save_path)?;
+                    fs::write(self.save_path.to_string(), content)?;
                 }
             }
         }
@@ -50,7 +57,7 @@ impl Logger {
         content = content.replace("\r", "");
         //verify
         if self.contents.len() == 0 {
-            self.contents.push(self.log_title.to_string());
+            self.contents.push(format!("[[{}]]",self.log_title));
             self.contents.push(content.to_string());
         }
         //more than 0 //cannot go below 0
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use debug::logger::Logger;
+
 fn main() {
     start();
 }
 
 fn start() {
     //caching
+    let mut test_logger = Logger::new("test logger", "logtest.log", true);
+    for i in 0..10 {
+        test_logger.add(&format!("test: {}", i));
+    }
+    test_logger.save().unwrap();
     let main_renderer = renderer::entry::ren_entry::MainNavigator;
     let url = String::from("http://127.0.0.1:5500/");
     //liverserver's url
